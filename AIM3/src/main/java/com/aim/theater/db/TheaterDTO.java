6aa6d4e0a26f16cc4489eec796119d4534c09f1f
Administrator <Administrator@DESKTOP-LEMDH1F>
2022-11-09 19:42:27 +0900

    - 극장 db

--- AIM3/src/main/java/com/aim/theater/db/TheaterDTO.java
+++ AIM3/src/main/java/com/aim/theater/db/TheaterDTO.java
@@ -14,6 +14,104 @@ public class TheaterDTO {
 	private String branch_name;
 	private String branch_addr;
 	
+	
+	
+	public int getScCode() {
+		return scCode;
+	}
+
+
+
+	public void setScCode(int scCode) {
+		this.scCode = scCode;
+	}
+
+
+
+	public int getBranchCd() {
+		return branchCd;
+	}
+
+
+
+	public void setBranchCd(int branchCd) {
+		this.branchCd = branchCd;
+	}
+
+
+
+	public int getRoomCd() {
+		return roomCd;
+	}
+
+
+
+	public void setRoomCd(int roomCd) {
+		this.roomCd = roomCd;
+	}
+
+
+
+	public String getStarttime() {
+		return starttime;
+	}
+
+
+
+	public void setStarttime(String starttime) {
+		this.starttime = starttime;
+	}
+
+
+
+	public String getSc_date() {
+		return sc_date;
+	}
+
+
+
+	public void setSc_date(String sc_date) {
+		this.sc_date = sc_date;
+	}
+
+
+
+	public String getMovieNm() {
+		return movieNm;
+	}
+
+
+
+	public void setMovieNm(String movieNm) {
+		this.movieNm = movieNm;
+	}
+
+
+
+	public String getBranch_name() {
+		return branch_name;
+	}
+
+
+
+	public void setBranch_name(String branch_name) {
+		this.branch_name = branch_name;
+	}
+
+
+
+	public String getBranch_addr() {
+		return branch_addr;
+	}
+
+
+
+	public void setBranch_addr(String branch_addr) {
+		this.branch_addr = branch_addr;
+	}
+
+
+
 	@Override
 	public String toString() {
 		return "TheaterDTO [scCode=" + scCode + ", branchCd=" + branchCd + ", roomCd=" + roomCd + ", starttime="
